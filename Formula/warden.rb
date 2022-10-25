1e474beb4770596915724fe215866f42a9cbf301
Shakh Mehedi <shakhmehedi@rezolve.com>
2022-10-25 10:36:33 +0100

    disable basic auth for portainer

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -2,9 +2,9 @@
 class Warden < Formula
   desc "Warden is a CLI utility for working with docker-compose environments"
   homepage "https://warden.dev"
-  version "0.13.3"
-  url "https://github.com/shakh-rezolve/warden/archive/0.13.3.tar.gz"
-  sha256 "0796eb7c58e48ebc5c5b4b9053eb16367b5b041b8f827a4f21e4db2385a4d619"
+  version "0.13.4"
+  url "https://github.com/shakh-rezolve/warden/archive/0.13.4.tar.gz"
+  sha256 "7530554eb5049322a4ccd0e3cad0a9afc2a8e4223af2d307d5620730364d9cb0"
   head "https://github.com/shakh-rezolve/warden.git", :branch => "0.13.0-tag"
 
   def install
