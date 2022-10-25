93db925079c172aa9b736618617810041783c1d6
Shakh Mehedi <shakhmehedi@rezolve.com>
2022-10-25 10:17:34 +0100

    updated version

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -2,9 +2,9 @@
 class Warden < Formula
   desc "Warden is a CLI utility for working with docker-compose environments"
   homepage "https://warden.dev"
-  version "0.13.2"
-  url "https://github.com/shakh-rezolve/warden/archive/0.13.2.tar.gz"
-  sha256 "ba0bffb2d1e33dd453d57f75be3c0ffbeca1391db645beab23e2d31197e44dd8"
+  version "0.13.3"
+  url "https://github.com/shakh-rezolve/warden/archive/0.13.3.tar.gz"
+  sha256 "0796eb7c58e48ebc5c5b4b9053eb16367b5b041b8f827a4f21e4db2385a4d619"
   head "https://github.com/shakh-rezolve/warden.git", :branch => "0.13.0-tag"
 
   def install
